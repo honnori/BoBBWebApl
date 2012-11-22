ee686972c061de7fb2cb2180ab1fa7796b5a2c95
NORIKAZU TAKESUE <uzakiron.27105650@gmail.com>
2012-11-22 15:12:43 +0900

    test

--- db/migrate/20121118041504_create_users.rb
+++ db/migrate/20121118041504_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :user_name
-      t.primary_key :user_id
+#      t.primary_key :user_id
 
       t.timestamps
     end
