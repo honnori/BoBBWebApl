1f26f32afe91051a204f0cbeba4bc9365a6a7390
NORIKAZU TAKESUE <uzakiron.27105650@gmail.com>
2012-11-22 15:14:58 +0900

    test

--- db/migrate/20121118041825_create_battle_records.rb
+++ db/migrate/20121118041825_create_battle_records.rb
@@ -1,7 +1,7 @@
 class CreateBattleRecords < ActiveRecord::Migration
   def change
     create_table :battle_records do |t|
-      t.primary_key :battle_id
+#      t.primary_key :battle_id
       t.integer :req_user_id
       t.integer :res_user_id
       t.integer :battle_status
